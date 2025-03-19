be0190927649dc799c9b270f5a86037ebef05964
sasa2me <115647387+sasa2me@users.noreply.github.com>
2025-03-19 16:45:14 +0700

    add urls.py

--- web_app/members/views.py
+++ web_app/members/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """
+    A simple view to render the index page.
+    """
+    return render(request, 'index.html')
